811fda656e1cac104ff73334c5444cb7b04e8c99
ralkuj <58257233+ralkuj@users.noreply.github.com>
2025-02-25 09:47:39 +0100

    Update 01.SNP_simulations_Song&Zhang_20250225.R

--- 01.SNP_simulations_Song&Zhang_20250225.R
+++ 01.SNP_simulations_Song&Zhang_20250225.R
@@ -34,6 +34,7 @@ nrcores <- 50
 ### The simulation from Song&Zhang translated into R functions
 ### The code is adopted from accompanying code to Song & Zhang, accessed 2025-02-25:
 ### https://github.com/song88180/Human_sex_ratio/blob/main/02.2_Zietsch_power_analysis.ipynb
+### Some comments below kept from Song&Zhang
 
 ### Function for calculating cousin pair probabilities for sex combinations
 getprobfun <- function(beta=.1,MAF=.1,SR_WT=.52){
